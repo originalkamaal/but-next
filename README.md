3cd5ef532509b5b012fb3fa034bcd7bbf1351599
originalkamaal <56730770+originalkamaal@users.noreply.github.com>
2022-11-28 19:30:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# but-next
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-8djnxg)
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {
+    router.replace('/home');
+    console.log('it worked');
+  }, []);
+  const router = useRouter();
+  return <></>;
+}
