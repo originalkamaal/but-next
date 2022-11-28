334064dfd99c7fbac0d020b9d78ae166dc33f91c
Kamaal Khan <originalkamaal@gmail.com>
2022-11-29 00:47:58 +0530

    ok

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,33 @@
 import Layout from '../layouts/Main';
+
 import Banner from '../components/Banner';
+import WhyBrightUrban from '../containers/WhyBrightUrban'
+import OurServices from '../containers/OurServices'
+import BlogsHome from '../containers/BlogsHome'
+import OurAchievements from '../containers/OurAchievements'
+
+
 export default function Home() {
+
   return (
+
     <Layout>
+
       <Banner />
+      <WhyBrightUrban />
+
+      <OurServices />
+
+      <BlogsHome />
+
+      <OurAchievements />
+
+      
+
+
     </Layout>
+
   );
+
 }
+
