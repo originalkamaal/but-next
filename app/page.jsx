1665f6ca51b6dbd843c0c187a5de54c87fc3a746
originalkamaal <56730770+originalkamaal@users.noreply.github.com>
2022-11-28 19:35:44 +0530

    1 file modified, 1 added

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,11 @@ import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function Home() {
-  useEffect(() => {
-    router.replace('/home');
-    console.log('it worked');
-  }, []);
-  const router = useRouter();
-  return <></>;
+  // useEffect(() => {
+  //   router.replace('/home');
+  //   console.log('it worked');
+  // }, []);
+  // const router = useRouter();
+  // return <></>;
+  return <div>koasjdflkjlsdf</div>;
 }
