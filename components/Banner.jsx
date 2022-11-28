2b506b3c0b6762757690361156709ae5705e1b74
originalkamaal <56730770+originalkamaal@users.noreply.github.com>
2022-11-28 20:55:33 +0530

    1 file modified

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import BannerStats from '../assets/Hero.png';
+//import BannerStats from '../assets/Hero.png';
+import Image from 'next/image';
 
 const renderSwitch = ({
   title,
@@ -13,7 +14,12 @@ const renderSwitch = ({
       return (
         <div className="relative text-white w-full px-2 md:px-4">
           <a href="" className="w-full">
-            <img src={BannerStats} className="w-full" />
+            <Image
+              src="https://raw.githubusercontent.com/originalkamaal/but-react/main/src/assets/illustrations/Hero.png"
+              width={100}
+              height={60}
+              className="w-full"
+            />
           </a>
         </div>
       );
