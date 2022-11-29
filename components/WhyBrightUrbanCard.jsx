bcedd7a90db97f24702054c5d7f8d6f77328186e
Kamaal Khan <originalkamaal@gmail.com>
2022-11-29 07:48:27 +0530

    done

--- components/WhyBrightUrbanCard.jsx
+++ components/WhyBrightUrbanCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Image from "next/image"
 const WhyBrightUrbanCard = ({ logo, title, description }) => {
   return (
     <div className="w-full flex flex-col justify-center space-y-4 items-center px-4 pb-3">
       <div className="md:w-32 w-20">
-        <img src={logo} alt={title} />
+        <Image src={logo} alt={title} />
       </div>
       <div className="font-bold">{title}</div>
       <div className="md:w-[400px] text-center">{description}</div>
