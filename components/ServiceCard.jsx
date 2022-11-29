71161544f4eb92684c95c51ee73f686a8438444e
Kamaal Khan <originalkamaal@gmail.com>
2022-11-29 07:51:49 +0530

    ok

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+impoet Image from 'next/image'
 
 const ServiceCard = ({ data }) => {
   let icon = data.icon;
@@ -9,7 +10,7 @@ const ServiceCard = ({ data }) => {
     <div className="relative bg-gray-100 m-1 md:m-2 p-3 pb-[40px] md:pt-5 rounded-md flex-col justify-between">
       <div>
         <div className="flex w-full items-center">
-          <img src={icon} className="w-12 md:w-16 h-12 md:h-16" />
+          <Image src={icon} className="w-12 md:w-16 h-12 md:h-16" />
           <div className="md:text-2xl pl-2 font-extrabold text-budarkblue leading-4">
             {data.title}
           </div>
