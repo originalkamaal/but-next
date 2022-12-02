b6d1472fcbc16dfb5d344a035bbeab58b9ad3a56
originalkamaal <originalkamaal@gmail.com>
2022-12-02 10:24:28 +0000

    s

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -2,7 +2,8 @@ import nodemailer from "nodemailer"
 
 export const sendEmail = (options) => {
   const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
